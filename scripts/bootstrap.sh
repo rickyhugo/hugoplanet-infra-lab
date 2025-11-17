501656704833b2eb6cbb4f08d7075f9e2339253c
Hugo Englund <hugo.englund7@gmail.com>
2025-11-17 10:59:50 +0100

    chore: update bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,13 +2,14 @@
 
 helm install argo-cd charts/argo-cd/ --namespace argocd --create-namespace
 
+kubectl config set-context --current --namespace=argocd
+argocd login --core
 argocd app create apps \
   --dest-namespace argocd \
   --dest-server https://kubernetes.default.svc \
   --repo https://github.com/rickyhugo/hugoplanet-infra-lab.git \
+  --revision main \
   --path apps
 argocd app sync apps
 
-# helm template apps | kubectl apply -f -
-
-# kubectl delete secret -n argocd -l owner=helm,name=argo-cd
+kubectl delete secret -n argocd -l owner=helm,name=argo-cd
