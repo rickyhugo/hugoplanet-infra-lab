f404fc11923a35c399961aa843936033cc1df004
Hugo Englund <hugo.englund7@gmail.com>
2025-11-17 09:56:27 +0100

    fix: update repo url

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 
-helm install argo-cd charts/argo-cd/
-helm template apps | kubectl apply -f -
+helm install argo-cd charts/argo-cd/ --namespace argocd --create-namespace
 
-kubectl delete secret -n argo -l owner=helm,name=argo-cd
+argocd app create apps \
+  --dest-namespace argocd \
+  --dest-server https://kubernetes.default.svc \
+  --repo https://github.com/rickyhugo/hugoplanet-infra-lab.git \
+  --path apps
+argocd app sync apps
+
+# helm template apps | kubectl apply -f -
+
+# kubectl delete secret -n argocd -l owner=helm,name=argo-cd
