363d0c110574c67afdff1129a88779d6b4417210
Hugo Englund <hugo.englund7@gmail.com>
2025-11-17 09:43:46 +0100

    refactor: update structure

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-helm install argo-cd charts/argo-cd/ --namespace argo --create-namespace
-helm template charts/root-app/ --namespace argo | kubectl apply -f -
+helm install argo-cd charts/argo-cd/
+helm template apps | kubectl apply -f -
 
 kubectl delete secret -n argo -l owner=helm,name=argo-cd
