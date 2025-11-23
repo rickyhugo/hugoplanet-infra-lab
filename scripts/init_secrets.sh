ea2ec498689c3366e992834e876fe17d9b25115a
Hugo Englund <hugo.englund7@gmail.com>
2025-11-23 18:10:39 +0100

    feat: setup dns

--- scripts/init_secrets.sh
+++ scripts/init_secrets.sh
@@ -7,3 +7,10 @@ kubectl create secret generic operator-oauth \
   -n tailscale \
   --from-literal=client_id="$TAILSCALE_CLIENT_ID" \
   --from-literal=client_secret="$TAILSCALE_CLIENT_SECRET"
+
+kubectl -n cert-manager create secret generic cloudflare-api-token \
+  --from-literal=api-token="$CLOUDFLARE_API_TOKEN"
+
+kubectl create namespace external-dns
+kubectl -n external-dns create secret generic cloudflare-api-token \
+  --from-literal=api-token="$CLOUDFLARE_API_TOKEN"
